93eaf28dc6d082dfae24da51fce12b19724388c0
GrayCatLi <1836626251@qq.com>
2022-06-06 23:14:28 +0800

    frist commit

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const publicRoutes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: publicRoutes
+})
+
+export default router
